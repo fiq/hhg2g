0c17f210f0915d90e96c07e5c11d262fa263ddd0
Raf Gemmail <raf@dreamthought.com>
2017-03-06 13:37:47 +1300

    Implemented Betelgeuse V lookup

--- src/hiker.js
+++ src/hiker.js
@@ -1,11 +1,13 @@
 "use strict";
 
 var planets = {
-  'earth': 'Mostly harmless'
+  'earth': 'Mostly harmless',
+  'betelgeuse v': 'A small and delightful planet in the vicinity of Betelgeuse.',
 };
 
 function lookup(place) {
-  return planets['earth'];
+  // FIXME - handle failed lookup
+  return planets[place];
 }
 
 module.exports = {
--- test/hiker.js
+++ test/hiker.js
@@ -1,9 +1,14 @@
 "use strict";
-
 var expect    = require("chai").expect;
 var book      = require("../src/hiker.js");
 describe("Hitch Hikers Guide To The Galaxy", function() {
   it("Should describe Earth", ()=>{
-    expect(book.lookup("earth")).to.equal("Mostly harmless"); 	
+    expect(book.lookup("earth")).to.equal("Mostly harmless");
   });
+
+  it("Should describe Betelgeuse V", ()=>{
+    expect(book.lookup("betelgeuse v")).to.equal(
+      "A small and delightful planet in the vicinity of Betelgeuse."); 	
+  });
+
 });
