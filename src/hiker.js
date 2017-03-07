445ba50008ea8feb6837f42227ee409baa79cac1
Raf Gemmail <Raf Gemmail>
2017-03-07 22:52:44 +1300

    Added support for mars

--- src/hiker.js
+++ src/hiker.js
@@ -1,8 +1,10 @@
 "use strict";
 
+// TODO: Put in DB
 var planets = {
   'earth': 'Mostly harmless',
   'betelgeuse v': 'A small and delightful planet in the vicinity of Betelgeuse.',
+  'mars': 'The chances? A 1,000,000:1 they said.',
 };
 
 function lookup(place) {
--- test/hiker.js
+++ test/hiker.js
@@ -4,11 +4,15 @@ var expect    = require("chai").expect;
 var book      = require("../src/hiker.js");
 describe("Hitch Hikers Guide To The Galaxy", function() {
   it("Should describe Earth", ()=>{
-    expect(book.lookup("earth")).to.equal("Mostly harmless"); 	
+    expect(book.lookup("earth")).to.equal("Mostly harmless");
   });
 
   it("Should describe Betelgeuse V", ()=>{
     expect(book.lookup("betelgeuse v")).to.equal("A small and delightful planet in the vicinity of Betelgeuse.");
+  });
+
+  it("Should describe Mars", ()=>{
+    expect(book.lookup("mars")).to.equal("The chances? A 1,000,000:1 they said.");
   });
 
 });
