5e36dd59eed7e2f8eeba33b0d3297ed273a24ea9
Raf Gemmail <Raf Gemmail>
2017-03-07 22:28:19 +1300

    completed

--- src/hiker.js
+++ src/hiker.js
@@ -1,11 +1,12 @@
 "use strict";
 
 var planets = {
-  'earth': 'Mostly harmless'
+  'earth': 'Mostly harmless',
+  'betelgeuse v': 'A small and delightful planet in the vicinity of Betelgeuse.',
 };
 
 function lookup(place) {
-  return planets['earth'];
+  return planets[place];
 }
 
 module.exports = {
--- test/hiker.js
+++ test/hiker.js
@@ -6,4 +6,9 @@ describe("Hitch Hikers Guide To The Galaxy", function() {
   it("Should describe Earth", ()=>{
     expect(book.lookup("earth")).to.equal("Mostly harmless"); 	
   });
+
+  it("Should describe Betelgeuse V", ()=>{
+    expect(book.lookup("betelgeuse v")).to.equal("A small and delightful planet in the vicinity of Betelgeuse.");
+  });
+
 });
